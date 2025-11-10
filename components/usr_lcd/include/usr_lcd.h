a1b6d4641b16244040628c75506174442ccfa3e2
Solonix-Chu <1468559561@qq.com>
2025-11-10 17:26:17 +0800

    修复优化部分问题

--- components/usr_lcd/include/usr_lcd.h
+++ components/usr_lcd/include/usr_lcd.h
@@ -1,1 +1,15 @@
+#pragma once
+
+#include "u8g2.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern u8g2_t u8g2;
+
 void usr_lcd_init(void);
+
+#ifdef __cplusplus
+}
+#endif
--- components/usr_lcd/usr_lcd.c
+++ components/usr_lcd/usr_lcd.c
@@ -1,5 +1,4 @@
 #include <driver/gpio.h>
-#include <driver/spi_master.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -9,14 +8,18 @@
 
 #include "sdkconfig.h"
 #include "u8g2_esp32_hal.h"
+#include "usr_lcd.h"
 
-// SDA - GPIO21
+// SDA - GPIO5
 #define PIN_SDA 5
 
-// SCL - GPIO22
+// SCL - GPIO6
 #define PIN_SCL 6
 
-static const char* TAG = "ssd1306";
+static const char* TAG = "usr_lcd";
+
+// Define the global u8g2 object
+u8g2_t u8g2;
 
 void usr_lcd_init(void)
 {
@@ -25,34 +28,17 @@ void usr_lcd_init(void)
   u8g2_esp32_hal.bus.i2c.scl = PIN_SCL;
   u8g2_esp32_hal_init(u8g2_esp32_hal);
 
-  u8g2_t u8g2;  // a structure which will contain all the data for one display
+  // Use the global u8g2 object
   u8g2_Setup_ssd1306_i2c_128x32_univision_f(
       &u8g2, U8G2_R0,
-      // u8x8_byte_sw_i2c,
       u8g2_esp32_i2c_byte_cb,
-      u8g2_esp32_gpio_and_delay_cb);  // init u8g2 structure
+      u8g2_esp32_gpio_and_delay_cb);
   u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
 
-  ESP_LOGI(TAG, "u8g2_InitDisplay");
-  u8g2_InitDisplay(&u8g2);  // send init sequence to the display, display is in
-                            // sleep mode after this,
-
-  ESP_LOGI(TAG, "u8g2_SetPowerSave");
-  u8g2_SetPowerSave(&u8g2, 0);  // wake up display
-  ESP_LOGI(TAG, "u8g2_ClearBuffer");
+  ESP_LOGI(TAG, "Initializing display");
+  u8g2_InitDisplay(&u8g2);
+  u8g2_SetPowerSave(&u8g2, 0); 
   u8g2_ClearBuffer(&u8g2);
-  ESP_LOGI(TAG, "u8g2_DrawBox");
-  u8g2_DrawBox(&u8g2, 0, 26, 80, 6);
-  u8g2_DrawFrame(&u8g2, 0, 26, 100, 6);
-
-  ESP_LOGI(TAG, "u8g2_SetFont");
-  u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
-  ESP_LOGI(TAG, "u8g2_DrawStr");
-  u8g2_DrawStr(&u8g2, 2, 17, "Hi nkolban!");
-  ESP_LOGI(TAG, "u8g2_SendBuffer");
   u8g2_SendBuffer(&u8g2);
-
-  ESP_LOGI(TAG, "All done!");
-
-  vTaskDelete(NULL);
+  ESP_LOGI(TAG, "Display initialized");
 }
